6c6b08fdd3b41379013ca21a896bb4f4bee70993
LitovicMateo <litovicm@gmail.com>
2025-11-10 18:09:35 +0100

    edit team didnt correctly upload team logo

--- frontend/src/services/teams/updateTeam.ts
+++ frontend/src/services/teams/updateTeam.ts
@@ -2,9 +2,12 @@ import slugify from 'react-slugify';
 
 import { API_ROUTES } from '@/constants/routes';
 import { TeamFormData } from '@/schemas/team-schema';
+import { uploadSingleImage } from '@/utils/uploadSingleImage';
 import axios from 'axios';
 
 export const updateTeam = async ({ id, ...data }: { id: string } & TeamFormData) => {
+	const uploadedImageId = await uploadSingleImage(data.image);
+
 	const res = await axios.put(API_ROUTES.edit.team(id), {
 		data: {
 			name: data.name,
@@ -12,7 +15,7 @@ export const updateTeam = async ({ id, ...data }: { id: string } & TeamFormData)
 			short_name: data.short_name,
 			slug: slugify(data.name),
 			city: data.city,
-			image: data.image.id || null,
+			image: uploadedImageId,
 			country: data.country
 		}
 	});
