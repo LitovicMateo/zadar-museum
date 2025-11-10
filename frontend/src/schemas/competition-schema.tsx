48216ceb3b66f1617640212a215fbd290960ede0
LitovicMateo <litovicm@gmail.com>
2025-11-10 17:31:06 +0100

    Allow competition short_name to be 2 or 3 characters (frontend validation)

--- frontend/src/schemas/competition-schema.tsx
+++ frontend/src/schemas/competition-schema.tsx
@@ -2,18 +2,10 @@ import * as z from 'zod';
 
 export const competitionSchema = z.object({
 	name: z.string().min(1),
-	short_name: z.string().length(3),
-	alternate_names: z.array(z.object({ name: z.string().min(1), short_name: z.string().length(3) })),
+	// allow short_name of either 2 or 3 characters
+	short_name: z.string().min(2).max(3),
+	alternate_names: z.array(z.object({ name: z.string().min(1), short_name: z.string().min(2).max(3) })),
 	trophies: z.array(z.string().length(4)) // years when won
 });
 
 export type CompetitionFormData = z.infer<typeof competitionSchema>;
-
-/*
-type CompetitionFormData = {
-	name: string;
-	short_name: string;
-	alternate_names: { name: string; short_name: string }[];
-	trophies: string[];
-};
-*/
